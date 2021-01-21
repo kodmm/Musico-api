1ee31e8399f0ea6d89fb5b5cc0665b4574fea5ba
kodmm <53547701+kodmm@users.noreply.github.com>
2021-01-21 12:17:44 +0900

    Created musicoDatabase && Add customersTables(sequelize db:migrate)

--- models/customer.js
+++ models/customer.js
@@ -3,23 +3,55 @@ const {
   Model
 } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
-  class Customer extends Model {
-    /**
-     * Helper method for defining associations.
-     * This method is not a part of Sequelize lifecycle.
-     * The `models/index` file will call this method automatically.
-     */
-    static associate(models) {
-      // define association here
+  //   class Customer extends Model {
+  //     /**
+  //      * Helper method for defining associations.
+  //      * This method is not a part of Sequelize lifecycle.
+  //      * The `models/index` file will call this method automatically.
+  //      */
+  //     static associate(models) {
+  //       // define association here
+  //     }
+  //   };
+  //   Customer.init({
+  //     name: DataTypes.STRING,
+  //     email: DataTypes.STRING,
+  //     rememberToken: DataTypes.STRING
+  //   }, {
+  //     sequelize,
+  //     modelName: 'Customer',
+  //   });
+  //   return Customer;
+  // };
+
+  const Customer = sequelize.define('Customer', {
+    name: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: 'nameは必ず入力してください。'
+        }
+      }
+    },
+    email: {
+      type: DataTypes.STRING,
+      validate: {
+        isEmail: {
+          msg: 'メールアドレスを入力してください。'
+        }
+      }
+    },
+    password: {
+      type: DataTypes.STRING,
+      validate: {
+        notEmpty: {
+          msg: 'passwordを必ず入力してください'
+        }
+      }
     }
+  }, {});
+  Customer.associate = function(models) {
+    // associations can be defined here
   };
-  Customer.init({
-    name: DataTypes.STRING,
-    email: DataTypes.STRING,
-    rememberToken: DataTypes.STRING
-  }, {
-    sequelize,
-    modelName: 'Customer',
-  });
   return Customer;
 };
